2538085ec66e002b41cc713c409ea9b18d76ef26
Antonio Linares <alinares@fivetechsoft.com>
2024-03-29 14:15:21 +0100

    updated

--- go1.bat
+++ go1.bat
@@ -1,5 +1,6 @@
 @setlocal
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-rem set HB_USER_CFLAGS=-TP
+set HB_USER_CFLAGS=-TP /Gm- /GS /arch:AVX2 /fp:precise /Zc:inline /Zc:forScope
+set HB_USER_LDFLAGS=/INCREMENTAL:NO /NOLOGO /SUBSYSTEM:CONSOLE /TLBID:1 /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" /manifest:embed /DYNAMICBASE /NXCOMPAT
 c:\harbour\bin\win\msvc64\hbmk2.exe test1.prg -lllama64
 @endlocal
